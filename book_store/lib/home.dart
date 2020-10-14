d5f96d5c12b087f872c37aba4000ef51d27062ef
waithethtetoo <4itcourse@gmail.com>
2020-10-14 13:24:27 +0630

    Display Books
    Display Books in GridView with a card

--- book_store/lib/home.dart
+++ book_store/lib/home.dart
@@ -1,26 +1,55 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Book Store',
-      home: HomePage(),
-    );
+  List<Card> _buildCardCollection(int count) {
+    List<Card> cards = List.generate(
+        count,
+        (index) => Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 18.0 / 11.0,
+                    child: Image.asset('assets/readBook.jpg'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Book Title'),
+                        SizedBox(height: 8.0),
+                        Text('Author'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ));
+    return cards;
   }
-}
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
+      appBar: AppBar(
+        title: Text('Book Store'),
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu Button');
+          },
+        ),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildCardCollection(10),
       ),
       resizeToAvoidBottomInset: false,
     );
