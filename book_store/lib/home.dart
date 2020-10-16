e6c77fa5e1c11911691a8ff8d17e12cc85fb60ec
waithethtetoo <4itcourse@gmail.com>
2020-10-16 13:03:19 +0630

    Retrieve book data from JSON
    get book data from php JSON

--- book_store/lib/home.dart
+++ book_store/lib/home.dart
@@ -1,35 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'book.dart';
 
 class Home extends StatelessWidget {
-  List<Card> _buildCardCollection(int count) {
-    List<Card> cards = List.generate(
-        count,
-        (index) => Card(
-              clipBehavior: Clip.antiAlias,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  AspectRatio(
-                    aspectRatio: 18.0 / 11.0,
-                    child: Image.asset('assets/readBook.jpg'),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Book Title'),
-                        SizedBox(height: 8.0),
-                        Text('Author'),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ));
-    return cards;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,13 +18,68 @@ class Home extends StatelessWidget {
           },
         ),
       ),
+      body: FutureBuilder<List<Book>>(
+        future: fetchBook(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+          return snapshot.hasData
+              ? BooksList(bookLst: snapshot.data)
+              : Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
+
+class BooksList extends StatelessWidget {
+  final List<Book> bookLst;
+
+  BooksList({Key key, this.bookLst}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
       body: GridView.count(
         crossAxisCount: 2,
         padding: EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        children: _buildCardCollection(10),
+        children: _buildCardCollection(bookLst.length),
       ),
       resizeToAvoidBottomInset: false,
     );
+  }
+
+  List<Card> _buildCardCollection(int count) {
+    List<Card> cards = List.generate(
+        count,
+        (index) => Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                      aspectRatio: 18.0 / 11.0,
+                      child: Image.network(
+                        bookLst[index].cover,
+                        width: 14.0,
+                      )),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          bookLst[index].title,
+                        ),
+                        SizedBox(height: 4.0),
+                        Text(bookLst[index].author),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ));
+    return cards;
   }
 }
