9510bfe27449412c1e30e81423648dbed6d05292
waithethtetoo <4itcourse@gmail.com>
2020-10-16 13:00:47 +0630

    Add new file (book.dart)
    Add new file for Book object

---
+++ book_store/lib/book.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Book {
+  final String title;
+  final String author;
+  final String cover;
+
+  Book({this.title, this.author, this.cover});
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+        title: json['title'], author: json['author'], cover: json['cover']);
+  }
+}
+
+List<Book> parseBook(String responseBody) {
+  final parse = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+  return parse.map<Book>((json) => Book.fromJson(json)).toList();
+}
+
+Future<List<Book>> fetchBook(http.Client client) async {
+  final response =
+      await http.get('http://192.168.56.1/book_store/books-json.php');
+  if (response.statusCode == 200) {
+    return compute(parseBook, response.body); // run in Background
+  } else {
+    throw Exception('Failed to load');
+  }
+}
